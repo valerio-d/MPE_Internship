3295802a6a7e89e240c7923fd88feab59245f78e
valerio-d <174138080+valerio-d@users.noreply.github.com>
2026-06-16 15:13:26 +0200

    WORKING final synthetic image with all neighbors (including non-psf) and original object execlution

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from astropy.wcs import WCS
 
 import numpy as np
 
-from scipy.ndimage import shift
+from scipy.ndimage import shift, zoom
 
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
@@ -16,6 +16,10 @@ from matplotlib.colors import LogNorm
 main_image_cutout = r"FITS\cutout_192.6009_-28.2675.fits" #future use input()?
 main_image_psf = r"FITS\copsf_192.6009_-28.2675.fits"
 neighbors_image = r"FITS\cat-ls-dr10.fits"
+
+#define the object we are interested in
+main_object_ra = 192.6009
+main_object_dec = -28.2675
 
 # -- Open File & Plot (cutout) --
 with fits.open(main_image_cutout) as hdu_list: #use with to safely close the file
@@ -36,7 +40,7 @@ plt.show()
 # -- Inspecting the PSF --
 with fits.open(main_image_psf) as psf_hdu_list:
     psf_hdu_list.info()
-    psf_data = psf_hdu_list[0].data * 1.3
+    psf_data = psf_hdu_list[0].data * 644 #found this multiplication factor by dividing brightest point on og image by the brightest point on psf image
 
     print('PSF shape:', psf_data.shape) #the dimentions of the array -> NAXIS1 is 1D Arrays, NAXIS2 is 2D Arrays, NAXIS3 is 3D Arrays
     print('PSF sum:', psf_data.sum()) # A PSF that sums to 1.0 means: "if I stamp this at a location and scale it by flux F, the total flux contributed to the image equals F."
@@ -52,7 +56,8 @@ neighbors = Table.read(neighbors_image) #Table.read already handles the closing 
 
 print(np.unique(neighbors['type'])) #1. check the different exitsting types (e.g., PSF, EXP, REX, etc.)
 
-psf_neighbors_sources = neighbors[neighbors['type'] == b'PSF'] #2. Filter for PSF types ONLY
+psf_neighbors_sources = neighbors #2. Filter for PSF types ONLY
+#psf_neighbors_sources = neighbors[neighbors['type'] == b'PSF'] #2. Filter for PSF types ONLY
 print(f"Total Neighbors: {len(neighbors)}")
 print(f"Total PSF Sources: {len(psf_neighbors_sources)}")
 
@@ -108,6 +113,17 @@ print(fluxes) #print all the aperture fluxes in the r band
 
 
 # -- PSF SYNTHETIC IMAGE CREATION AND PLACEMENT FUNCTION --
+#find and exclude the main object 
+main_object_distances = np.sqrt((filtered_psf_neighbors['ra'] - main_object_ra)**2 + (filtered_psf_neighbors['dec'] - main_object_dec)**2) #the distance from every source to the object we're interested in
+main_obj_index = np.argmin(main_object_distances) #find the index of the main object by finding the smallest distance
+
+psf_object_KEEP = np.arange(len(filtered_psf_neighbors)) != main_obj_index
+
+xs_without_target = xs[psf_object_KEEP]
+ys_without_target = ys[psf_object_KEEP]
+fluxes_without_target = filtered_psf_neighbors['flux_r'][psf_object_KEEP]
+
+
 def create_synthetic_image(image_shape, psf, sources_x, sources_y, fluxes): #takes the real size of the image, the psf stamp, the xs and xy arrays from the WCS conversion, and the fluxes (how bright each star is)
     synthetic = np.zeros(image_shape, dtype=np.float64) # creating a BLANK CANVAS, a 256 x 256 grid
     psf_h, psf_w = psf.shape
@@ -115,18 +131,21 @@ def create_synthetic_image(image_shape, psf, sources_x, sources_y, fluxes): #tak
 
     img_h, img_w = image_shape
 
-    for x, y, flux in zip(sources_x, sources_y, fluxes): #zip allows to combine the 3 lists in triplets (x1, y1. flux1), (x2, y2, flux2), etc.
+    for x, y, flux in zip(sources_x, sources_y, fluxes): #zip allows to combine the 3 lists in triplets (x1, y1. flux1), (x2, y2, flux2), etc. #this means that we are going through each one of the identified objects
+
         ix, iy = int(round(x)), int(round(y)) #round the pixel position to the nearest pixel
+        print(ix, iy, flux)
 
         # calculate where the PSF stamp lands in the image, centering it on the star position
+        #Say the star is at pixel x=135, and the sticker is 25 pixels wide with center at index 12. If you want the sticker's center pixel to land exactly on x=135, the sticker's left edge (x0) must start at 135 - 12 = 123. Then the sticker's right edge (x1) is 123 + 25 = 148. So the sticker occupies columns 123 through 148 in the big image, and its middle column (135) lines up exactly with the star.
         x0 = ix - psf_cx
         y0 = iy - psf_cy
         x1 = x0 + psf_w
         y1 = y0 + psf_h
         
         # Clip to image boundaries
-        img_x0 = max(x0, 0);  img_x1 = min(x1, img_w)
-        img_y0 = max(y0, 0);  img_y1 = min(y1, img_h)
+        img_x0 = max(x0, 0);  img_x1 = min(x1, img_w) #if the column < 0, set it = 0
+        img_y0 = max(y0, 0);  img_y1 = min(y1, img_h) #the right edge of the psf cannot go past the image's width
         psf_x0 = img_x0 - x0; psf_x1 = psf_x0 + (img_x1 - img_x0)
         psf_y0 = img_y0 - y0; psf_y1 = psf_y0 + (img_y1 - img_y0)
         
@@ -137,7 +156,7 @@ def create_synthetic_image(image_shape, psf, sources_x, sources_y, fluxes): #tak
     
     return synthetic
 
-synthetic_image = create_synthetic_image(r_band.shape, psf_data, xs, ys, fluxes) 
+synthetic_image = create_synthetic_image(r_band.shape, psf_data, xs_without_target, ys_without_target, fluxes_without_target) #create image WITHOUT the main object
 synthetic_image_data = synthetic_image.data
 #assign the arguments: image shape (the r-band shape), the psf data, the xs and ys coordinates for each psf, and the fluxes for each psf
 
@@ -182,7 +201,6 @@ cbar_1.ax.set_yticklabels([f"{int(low_tick):,}", f"{int(medium_tick):,}", f"{int
 cbar_2.ax.set_yticklabels([f"{int(low_tick):,}", f"{int(medium_tick):,}", f"{int(high_tick):,}"])
 
 plt.tight_layout()
-plt.show()
 
 # print(header) print(psf_hdu_list[0].header) print(wcs)
 
@@ -212,3 +230,41 @@ plt.legend()
 plt.grid(alpha=0.3)
 plt.show()
 
+print(psf_data.shape)
+
+# -- RESIZE THE PSF IMAGE TO THE CORRECT RESOLUTION --
+psf_data_enlarged = zoom(psf_data, 4, order=3) #the image to enlarge, the scale factor, the order (cubic interpolation allows for a smooth result)
+psf_data_enlarged = psf_data_enlarged / psf_data_enlarged.sum() * psf_data.max() #renormalize everything to ensure that sum = 1
+
+print(psf_data_enlarged.sum()) #verify sum = 1
+
+#replot
+synthetic_image = create_synthetic_image(r_band.shape, psf_data_enlarged, xs_without_target, ys_without_target, fluxes)
+fig, axes = plt.subplots(1, 2, figsize=(16, 8))
+LogNorm_minimum_value = 0.1 #minumum value to handle fully black
+LogNorm_maximum_value = r_band.max() #minumum value to handle fully black
+
+logarithmic_image1 = axes[0].imshow(r_band, origin="lower", norm=LogNorm(vmin=LogNorm_minimum_value, vmax=LogNorm_maximum_value), cmap="gray")
+axes[0].set_title('original r-band image')
+logarithmic_image2 = axes[1].imshow(synthetic_image, origin="lower", norm=LogNorm(vmin=LogNorm_minimum_value, vmax=LogNorm_maximum_value), cmap="gray")
+axes[1].set_title('synthetic PSF image')
+
+
+low_tick = np.percentile(r_band[r_band > 0], 20) #ticks for image 1
+medium_tick = np.percentile(r_band[r_band > 0], 50)#calculate percentiles using only positive pixels to avoid math issues
+high_tick = np.percentile(r_band[r_band > 0], 90)
+low_tick2 = np.percentile(synthetic_image[synthetic_image > 0], 20) #ticks for image 2
+medium_tick2 = np.percentile(synthetic_image[synthetic_image > 0], 50)
+high_tick2 = np.percentile(synthetic_image[synthetic_image > 0], 90)
+
+
+cbar_1 = fig.colorbar(logarithmic_image1, ax=axes[0], ticks=[low_tick, medium_tick, high_tick]) # Create specific colorbars for each of the images
+cbar_2 = fig.colorbar(logarithmic_image2, ax=axes[1], ticks=[low_tick, medium_tick, high_tick]) 
+# Format the labels
+cbar_1.ax.set_yticklabels([f"{int(low_tick):,}", f"{int(medium_tick):,}", f"{int(high_tick):,}"])
+cbar_2.ax.set_yticklabels([f"{int(low_tick):,}", f"{int(medium_tick):,}", f"{int(high_tick):,}"])
+
+plt.tight_layout()
+plt.show()
+
+print(neighbors['shape_r'])
